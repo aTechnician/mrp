43fe853b7b4e8d8cc92ed01d88f9d6f5028259ac
a.duan <atechnician@outlook.com>
2025-09-18 16:06:17 -0400

    started OCR shellscripts

---
+++ bin/ocr-pdfs.sh
@@ -0,0 +1,5 @@
+# Tesseract shellscript to embedd OCR into war diaries
+# expected input
+# [script name] [file name]
+
+
---
+++ bin/wd-grep.sh
@@ -0,0 +1,2 @@
+# Script to remove spaces and grep -i files for key words post OCR.  For use 
+# with primary sources.  rm spaces with tr -d, pipe into grep
