42ccf997342efc8218edbe66796b6ea071d7bb22
a.duan <atechnician@outlook.com>
2025-09-29 22:41:33 -0400

    Database  inited

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE ww2_log;
+
 USE ww2_log;
 
 CREATE TABLE unit_types(
@@ -23,11 +25,18 @@ nme varchar(15) NOT NULL,
 num VARCHAR(5),
 size VARCHAR(5) NOT NULL,
 parent INT,
-PRIMARY KEY (unit),
+PRIMARY KEY (uic),
 FOREIGN KEY (size) REFERENCES unit_types(ident),
-FOREIGN KEY (parent) REFERENCES units(parent)
+FOREIGN KEY (parent) REFERENCES units(uic)
 );
 
+CREATE TABLE unit_notes (
+	id INT NOT NULL AUTO_INCREMENT,
+    uic INT,
+    note TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (uic) REFERENCES units(uic)
+);
 
 CREATE TABLE fonds_tna (
 originator VARCHAR (5) NOT NULL,
@@ -38,8 +47,17 @@ e_date DATE,
 ancestry BOOL,
 requested BOOL,
 scanned BOOL,
+filed BOOL,
 fond VARCHAR(16) AS (CONCAT(originator,section,"/",folder)) STORED,
 PRIMARY KEY(fond)
+);
+
+CREATE TABLE fonds_tna_notes (
+	id INT NOT NULL AUTO_INCREMENT,
+    fond VARCHAR(16),
+    note TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (fond) REFERENCES fonds_tna(fond)
 );
 
 CREATE TABLE unit_fonds (
@@ -49,3 +67,4 @@ PRIMARY KEY (uic, fond),
 FOREIGN KEY (uic) REFERENCES units(uic),
 FOREIGN KEY (fond) REFERENCES fonds_tna(fond)
 );
+
